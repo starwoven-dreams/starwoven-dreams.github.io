6bdf14c44fdb0cf04196c1dd5617a964d9c0fcfd
starwoven-dreams <oneattic@gmail.com>
2025-07-05 23:20:02 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from 'react-router';
+import Home from '@/pages/Home';
+
+function App() {
+  return (
+    <Routes>
+      <Route index element={<Home />} />
+    </Routes>
+  );
+}
+
+export default App;
