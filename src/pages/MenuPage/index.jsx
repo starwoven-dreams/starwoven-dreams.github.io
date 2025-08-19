12c3caa7fd842a68cdc24b70a71e62e8b3bdee23
starwoven-dreams <oneattic@gmail.com>
2025-08-19 20:51:55 +0900

    배경 안보이는 거 이제 수정해줄래

--- src/pages/MenuPage/index.jsx
+++ src/pages/MenuPage/index.jsx
@@ -1,6 +1,6 @@
 function MenuPage() {
   return (
-    <div className="min-h-screen bg-[url('./images/background_main.png')] bg-cover bg-center flex items-center justify-center">
+    <div className="min-h-screen bg-[url('/images/background_main.png')] bg-cover bg-center flex items-center justify-center">
       <div className="p-8 rounded flex flex-col items-center">
         <img src="images/logo.png" alt="logo" className="m-4" />
       </div>
